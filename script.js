3358d1723f8a4cd2ceea2c68bbb300ef3ab48e90
Lordobe <niklas.aeschbach@icloud.com>
2026-03-10 15:00:38 +0100

    Issue 3 (refs #23)

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-//Saison-Liste generieren (1950 bis aktuelles Jahr)
+
 
 const seasonsContainer = document.getElementById("seasons-container");
 
 const firstSeason = 1950;
 const currentYear = new Date().getFullYear();
 
-// überschrift für auswahl der saison
+
 const title = document.createElement("h2");
 title.textContent = "Season auswählen";
 seasonsContainer.appendChild(title);
 
-// Container für Buttons/Cards
+// Container für die Buttons
 const seasonsGrid = document.createElement("div");
 seasonsGrid.id = "seasons-grid";
 seasonsContainer.appendChild(seasonsGrid);
 
 // Jahre absteigend anzeigen 
 for (let year = currentYear; year >= firstSeason; year--) {
+
   const seasonBtn = document.createElement("button");
   seasonBtn.className = "season-button";
   seasonBtn.textContent = year;
 
-  // Klick event muss noch hinzugefügt werden Robin
+  // Issue 3: Navigation zur Season-Seite
+  seasonBtn.addEventListener("click", function () {
+    window.location.href = "season.html?season=" + year;
+  });
 
   seasonsGrid.appendChild(seasonBtn);
 }
