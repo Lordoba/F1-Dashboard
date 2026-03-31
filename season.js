3c8250e242757e11074aabca88b2867eba324095
Lordobe <niklas.aeschbach@icloud.com>
2026-03-31 15:34:04 +0200

    Konstruktörs (refs #29 und refs #31)

--- season.js
+++ season.js
@@ -7,18 +7,49 @@ seasonTitle.textContent = "Season " + currentSeason;
 fetch(`https://api.jolpi.ca/ergast/f1/${currentSeason}/constructorstandings/`)
     .then(response => response.json())
     .then(data => {
-        console.log("Season data:", data);
+        const lists = data.MRData.StandingsTable.StandingsLists;
+
+        // Check ob Daten existieren
+        if (lists.length === 0) {
+            console.log("Keine Constructor-Daten für diese Saison");
+
+            const tableBody = document.getElementById("constructorBody");
+            tableBody.innerHTML = "<tr><td colspan='3'>Keine Daten verfügbar</td></tr>";
+
+            return;
+        }
+
         const standings = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-        console.log("Season standings:", standings);
+        const tableBody = document.getElementById("constructorBody");
+        tableBody.innerHTML = ""; // Tabele leeren bevor neue Daten hinzugefügt werden
 
         for (const team of standings) {
-            console.log("Team:", team);
-            console.log("Points:", team.points);
+
+            // Zeile erstellen
+            const row = document.createElement("tr");
+
+            // Position
+            const positionCell = document.createElement("td");
+            positionCell.textContent = team.position;
+
+            // Team Name
+            const teamCell = document.createElement("td");
+            teamCell.textContent = team.Constructor.name;
+
+            // Punkte
+            const pointsCell = document.createElement("td");
+            pointsCell.textContent = team.points;
+
+            // hinzufügen
+            row.appendChild(positionCell);
+            row.appendChild(teamCell);
+            row.appendChild(pointsCell);
+
+            // Zeile zur Tabelle hinzufügen
+            tableBody.appendChild(row);
         }
     })
     .catch(error => {
         console.error("Error fetching season data:", error);
-        // const standingsContainer = document.getElementById("standings-container");
-        // standingsContainer.textContent = "Fehler beim Laden der Daten.";
     });
 
